68632cb832f7927e975c01c203e90836bcd328df
moll-y <mol.tired@gmail.com>
2021-09-01 15:06:18 -0500

    add items module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ItemsModule } from './items/items.module';
 
 @Module({
-	imports: [TypeOrmModule.forRoot()],
+  imports: [TypeOrmModule.forRoot(), ItemsModule],
 })
 export class AppModule {}
