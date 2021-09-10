98c9ad6327082306f07b3ca0d12611323e023516
moll-y <mol.tired@gmail.com>
2021-09-10 13:53:32 -0500

    fetch all by paginated

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+
 import { Item } from './entities/item.entity';
+import { ItemPaginatedRequestDto } from './dto/item-paginated-request.dto';
+import { ItemPaginatedResponseDto } from './dto/item-paginated-response.dto';
 
 @Injectable()
 export class ItemsService {
@@ -13,8 +16,21 @@ export class ItemsService {
   async create(item: Item): Promise<Item> {
     return await this.itemsRepository.save(item);
   }
-  
-  async findAll(): Promise<Item[]> {
-    return await this.itemsRepository.find();
+
+  async findAll(itemPaginatedRequestDto): Promise<ItemPaginatedResponseDto> {
+    const page: number = itemPaginatedRequestDto.page;
+    const limit: number = itemPaginatedRequestDto.limit;
+
+    const [data, total] = await this.itemsRepository.findAndCount({
+      skip: (page - 1) * limit,
+      take: limit,
+    });
+
+    return {
+      data,
+      total,
+      limit,
+      page,
+    } as ItemPaginatedResponseDto;
   }
 }
